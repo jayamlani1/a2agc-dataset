4512c9fff400e81a1f1397451e6dc0a54bf37f15
Bruce Herr II <bherr@indiana.edu>
2019-07-05 16:30:38 -0400

    Cleaned up the opioid deaths sql, added comments, and removed excess data not used

--- src/create-vis-geomap-opioid-deaths-data.sql
+++ src/create-vis-geomap-opioid-deaths-data.sql
@@ -1,28 +1,42 @@
-SELECT DISTINCT
+SELECT
+    -- Data Variables
     d.CASE_NUMBER,
-    d.LAST_NAME,
-    d.FIRST_NAME,
     d.SEX,
-    CASE WHEN d.SEX = 'M' THEN 'blue'
-        WHEN d.SEX = 'F' THEN 'pink'
-        ELSE 'black' END AS 'SEX$$color',
-    d.HOME_ZIP,
     d.HOME_STATE,
-    CASE WHEN d.HOME_STATE = 'IN' THEN '#bebebe'
-        ELSE 'black' END AS 'HOME_STATE$$color',
-    CASE WHEN d.HOME_STATE = 'IN' THEN 'circle'
-        ELSE 'triangle' END AS 'HOME_STATE$$shape',
-    d.INJURY_ZIP,
     r.N_OPIOID_PRESCRIPTIONS,
-    (CAST(r.N_OPIOID_PRESCRIPTIONS AS real) / (SELECT max(N_OPIOID_PRESCRIPTIONS) FROM rollup1) * 500) + 25 AS 'N_OPIOID_PRESCRIPTIONS$$areaSize',
     l.LATITUDE,
     l.LONGITUDE,
+
+    -- Graphic Variables
+    ---- Color coding by SEX
+    CASE 
+        WHEN d.SEX = 'M' THEN 'blue'
+        WHEN d.SEX = 'F' THEN 'pink'
+    ELSE 'black' END AS 'SEX$$color',
+    ---- Color coding by HOME_STATE
+    CASE
+        WHEN d.HOME_STATE = 'IN' THEN '#bebebe'
+        ELSE 'black'
+    END AS 'HOME_STATE$$color',
+    ---- Shape coding by HOME_STATE
+    CASE 
+        WHEN d.HOME_STATE = 'IN' THEN 'circle'
+        ELSE 'triangle'
+    END AS 'HOME_STATE$$shape',
+    ---- Size coding by N_OPIOID_PRESCRIPTIONS
+    (CAST(r.N_OPIOID_PRESCRIPTIONS AS real) / (SELECT max(N_OPIOID_PRESCRIPTIONS) FROM rollup1) * 500) + 25 AS 'N_OPIOID_PRESCRIPTIONS$$areaSize',
+
+    ---- Fixed-value Graphic Variables
     'Fixed values for visualization' AS 'Fixed',
     '#bebebe' AS 'Fixed$$color',
     1 AS 'Fixed$$strokeWidth',
     0.4 AS 'Fixed$$strokeTransparency',
     0.2 AS 'Fixed$$transparency'
-FROM deaths as d
+FROM
+    deaths as d
+    -- Join rollup1 for N_OPIOID_PRESCRIPTIONS
     INNER JOIN rollup1 as r ON d.CASE_NUMBER = r.CASE_NUMBER
+    -- Join locations to get latitude/longitude of INJURY_ADDRESSes
     INNER JOIN locations as l ON d.INJURY_ADDRESS = l.ADDRESS and d.INJURY_CITY = l.CITY and d.INJURY_ZIP = l.ZIP
+
 ORDER BY d.CASE_NUMBER ASC;
