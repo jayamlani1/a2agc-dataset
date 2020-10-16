9c92083de1976633132d4a1e54a37a4b22f5f21f
Daniel Bolin <axbolin@iu.edu>
2020-10-16 12:22:51 -0400

    Added material + state setup

--- a2agc/src/app/app.module.ts
+++ a2agc/src/app/app.module.ts
@@ -1,17 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StateModule } from './state/state.module';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+
+    AppRoutingModule,
+    StateModule,
   ],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
