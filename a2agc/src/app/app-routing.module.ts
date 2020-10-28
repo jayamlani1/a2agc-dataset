5a115ed48ea3c2c53a49881004f8a4fc6e80e934
Adam Phillips <adjp2013@gmail.com>
2020-10-28 13:14:32 -0400

    changes to geomap for new site change

--- a2agc/src/app/app-routing.module.ts
+++ a2agc/src/app/app-routing.module.ts
@@ -7,11 +7,10 @@ const routes: Routes = [
     path: 'geomap',
     loadChildren: () => import('./pages/geomap/geomap.module').then(m => m.GeomapModule)
   },
-<<<<<<< Updated upstream
-  { path: 'vis1-geomap-of-opioid-deaths', loadChildren: () => import('./pages/vis1-geomap-of-opioid-deaths/vis1-geomap-of-opioid-deaths.module').then(m => m.Vis1GeomapOfOpioidDeathsModule) }
-=======
-  { path: 'vis3-heatmap-of-accidental-overdoses', loadChildren: () => import('./pages/vis3-heatmap-of-accidental-overdoses/vis3-heatmap-of-accidental-overdoses.module').then(m => m.Vis3HeatmapOfAccidentalOverdosesModule) }
->>>>>>> Stashed changes
+  {
+    path: 'vis1-geomap-of-opioid-deaths',
+    loadChildren: () => import('./pages/vis1-geomap-of-opioid-deaths/vis1-geomap-of-opioid-deaths.module').then(m => m.Vis1GeomapOfOpioidDeathsModule)
+  }
 ];
 
 @NgModule({
