7c856f763fa40e5d0174fa3a1b5641e3819284ff
Bruce Herr II <bherr@indiana.edu>
2021-06-23 10:53:01 -0400

    Fix path to output folder

--- data-processor/scripts/13-aggregate-table-data.sh
+++ data-processor/scripts/13-aggregate-table-data.sh
@@ -2,4 +2,5 @@
 source constants.sh
 set -ev
 
-python3 ./src/tables/data.py $DB $SCHEMA $COLUMN_DISTRIBUTION_OVERRIDES -o $AGGREGATE_DATA -d $OUT/site-data -s ../../data
+python3 ./src/tables/data.py $DB $SCHEMA $COLUMN_DISTRIBUTION_OVERRIDES -o $AGGREGATE_DATA -d $OUT/site-data -s assets/generated/histogram-data
+
